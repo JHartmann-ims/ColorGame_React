ea2ddd81f75e1889c68fb16e96b3b93450103835
Julian Hartmann <julian.hartmann@students.ksba.ch>
2023-05-07 15:05:14 +0200

    correction to the text size for mobile

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ColorGame from './components/ColorGame';
 import './index.css';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App bg-color-game-dark font-Raleway text-base min-h-screen">
       <ColorGame />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='absolute bottom-0 w-full h-[80px]'>
+        <p>footer</p>
+    </div>
+  )
+}
+
+export default Footer
